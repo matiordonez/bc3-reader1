ede5aeb1ccae2e5a84995e0fb5c412bcc92b9732
Matias Ordoñez Enciso <matiordonez_@MacBook-Air-de-Matias.local>
2026-02-07 20:33:12 +0100

    Exportar export_to_xlsx_bytes y export_to_pdf_bytes en bc3_reader

--- bc3_reader/__init__.py
+++ bc3_reader/__init__.py
@@ -2,7 +2,11 @@
 """BC3 Reader - Lector y exportador de archivos FIEBDC BC3."""
 
 from .parser import BC3Parser, PresupuestoBC3
-from .exporters import export_to_xlsx, export_to_pdf
+from .exporters import export_to_xlsx, export_to_pdf, export_to_xlsx_bytes, export_to_pdf_bytes
 
 __version__ = '1.0.0'
-__all__ = ['BC3Parser', 'PresupuestoBC3', 'export_to_xlsx', 'export_to_pdf']
+__all__ = [
+    'BC3Parser', 'PresupuestoBC3',
+    'export_to_xlsx', 'export_to_pdf',
+    'export_to_xlsx_bytes', 'export_to_pdf_bytes'
+]
